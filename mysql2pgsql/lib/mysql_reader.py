c26d75036adeb88d12ca2ba170d991fa7168fc88
netragonlong <netragonlong@gmail.com>
2023-08-07 02:07:33 +0900

    Add project files

---
+++ mysql2pgsql/lib/mysql_reader.py
@@ -0,0 +1,252 @@
+from __future__ import with_statement, absolute_import
+
+import re
+from contextlib import closing
+
+import MySQLdb
+import MySQLdb.cursors
+
+
+re_column_length = re.compile(r'\((\d+)\)')
+re_column_precision = re.compile(r'\((\d+),(\d+)\)')
+re_key_1 = re.compile(r'CONSTRAINT `(\w+)` FOREIGN KEY \(`(\w+)`\) REFERENCES `(\w+)` \(`(\w+)`\)')
+re_key_2 = re.compile(r'KEY `(\w+)` \((.*)\)')
+re_key_3 = re.compile(r'PRIMARY KEY +\((.*)\)')
+
+
+class DB:
+    """
+    Class that wraps MySQLdb functions that auto reconnects
+    thus (hopefully) preventing the frustrating
+    "server has gone away" error. Also adds helpful
+    helper functions.
+    """
+    conn = None
+
+    def __init__(self, options):
+        args = {
+            'user': str(options.get('username', 'root')),
+            'db': options['database'],
+            'use_unicode': True,
+            'charset': 'utf8',
+            }
+
+        if options.get('password', None):
+            args['passwd'] = str(options.get('password', None))
+
+        if options.get('socket', None):
+            args['unix_socket'] = str(options['socket'])
+        else:
+            args['host'] = str(options.get('hostname', 'localhost'))
+            args['port'] = options.get('port', 3306)
+            args['compress'] = options.get('compress', True)
+
+        self.options = args
+
+    def connect(self):
+        self.conn = MySQLdb.connect(**self.options)
+
+    def close(self):
+        self.conn.close()
+
+    def cursor(self, cursorclass=MySQLdb.cursors.Cursor):
+        try:
+            return self.conn.cursor(cursorclass)
+        except (AttributeError, MySQLdb.OperationalError):
+            self.connect()
+            return self.conn.cursor(cursorclass)
+
+    def list_tables(self):
+        return self.query('SHOW TABLES;')
+
+    def query(self, sql, args=(), one=False, large=False):
+        return self.query_one(sql, args) if one\
+            else self.query_many(sql, args, large)
+
+    def query_one(self, sql, args):
+        with closing(self.cursor()) as cur:
+            cur.execute(sql, args)
+            return cur.fetchone()
+
+    def query_many(self, sql, args, large):
+        with closing(self.cursor(MySQLdb.cursors.SSCursor if large else MySQLdb.cursors.Cursor)) as cur:
+            cur.execute(sql, args)
+            for row in cur:
+                yield row
+
+
+class MysqlReader(object):
+
+    class Table(object):
+        def __init__(self, reader, name):
+            self.reader = reader
+            self._name = name
+            self._indexes = []
+            self._foreign_keys = []
+            self._triggers = []
+            self._columns = self._load_columns()
+            self._comment = self._load_table_comment()
+            self._load_indexes()
+            self._load_triggers()
+
+        def _convert_type(self, data_type):
+            """Normalize MySQL `data_type`"""
+            if data_type.startswith('varchar'):
+                return 'varchar'
+            elif data_type.startswith('char'):
+                return 'char'
+            elif data_type in ('bit(1)', 'tinyint(1)', 'tinyint(1) unsigned'):
+                return 'boolean'
+            elif re.search(r'^smallint.* unsigned', data_type) or data_type.startswith('mediumint'):
+                return 'integer'
+            elif data_type.startswith('smallint'):
+                return 'tinyint'
+            elif data_type.startswith('tinyint') or data_type.startswith('year('):
+                return 'tinyint'
+            elif data_type.startswith('bigint') and 'unsigned' in data_type:
+                return 'numeric'
+            elif re.search(r'^int.* unsigned', data_type) or \
+                    (data_type.startswith('bigint') and 'unsigned' not in data_type):
+                return 'bigint'
+            elif data_type.startswith('int'):
+                return 'integer'
+            elif data_type.startswith('float'):
+                return 'float'
+            elif data_type.startswith('decimal'):
+                return 'decimal'
+            elif data_type.startswith('double'):
+                return 'double precision'
+            else:
+                return data_type
+
+        def _load_columns(self):
+            fields = []
+            for row in self.reader.db.query('SHOW FULL COLUMNS FROM `%s`' % self.name):
+                res = ()
+                for field in row:
+                  if type(field) == unicode:
+                    res += field.encode('utf8'),
+                  else:
+                    res += field,
+                length_match = re_column_length.search(res[1])
+                precision_match = re_column_precision.search(res[1])
+                length = length_match.group(1) if length_match else \
+                    precision_match.group(1) if precision_match else None
+                name = res[0]
+                comment = res[8]
+                field_type = self._convert_type(res[1])
+                desc = {
+                    'name': name,
+                    'table_name': self.name,
+                    'type': field_type,
+                    'length': int(length) if length else None,
+                    'decimals': precision_match.group(2) if precision_match else None,
+                    'null': res[3] == 'YES' or field_type.startswith('enum') or field_type in ('date', 'datetime', 'timestamp'),
+                    'primary_key': res[4] == 'PRI',
+                    'auto_increment': res[6] == 'auto_increment',
+                    'default': res[5] if not res[5] == 'NULL' else None,
+                    'comment': comment,
+                    'select': '`%s`' % name if not field_type.startswith('enum') else
+                        'CASE `%(name)s` WHEN "" THEN NULL ELSE `%(name)s` END' % {'name': name},
+                    }
+                fields.append(desc)
+
+            for field in (f for f in fields if f['auto_increment']):
+                res = self.reader.db.query('SELECT MAX(`%s`) FROM `%s`;' % (field['name'], self.name), one=True)
+                field['maxval'] = int(res[0]) if res[0] else 0
+
+            return fields
+
+        def _load_table_comment(self):
+            table_status = self.reader.db.query('SHOW TABLE STATUS WHERE Name="%s"' % self.name, one=True)
+            comment = table_status[17]
+            return comment
+
+          
+        def _load_indexes(self):
+            explain = self.reader.db.query('SHOW CREATE TABLE `%s`' % self.name, one=True)
+            explain = explain[1]
+            for line in explain.split('\n'):
+                if ' KEY ' not in line:
+                    continue
+                index = {}
+                match_data = re_key_1.search(line)
+                if match_data:
+                    index['name'] = match_data.group(1)
+                    index['column'] = match_data.group(2)
+                    index['ref_table'] = match_data.group(3)
+                    index['ref_column'] = match_data.group(4)
+                    self._foreign_keys.append(index)
+                    continue
+                match_data = re_key_2.search(line)
+                if match_data:
+                    index['name'] = match_data.group(1)
+                    index['columns'] = [re.search(r'`(\w+)`', col).group(1) for col in match_data.group(2).split(',')]
+                    index['unique'] = 'UNIQUE' in line
+                    self._indexes.append(index)
+                    continue
+                match_data = re_key_3.search(line)
+                if match_data:
+                    index['primary'] = True
+                    index['columns'] = [re.sub(r'\(\d+\)', '', col.replace('`', '')) for col in match_data.group(1).split(',')]
+                    self._indexes.append(index)
+                    continue
+
+        def _load_triggers(self):
+            explain = self.reader.db.query('SHOW TRIGGERS WHERE `table` = \'%s\'' % self.name)
+            for row in explain:
+                if type(row) is tuple:
+                    trigger = {}
+                    trigger['name'] = row[0]
+                    trigger['event'] = row[1]
+                    trigger['statement'] = row[3]
+                    trigger['timing'] = row[4]
+
+                    trigger['statement'] = re.sub('^BEGIN', '', trigger['statement'])
+                    trigger['statement'] = re.sub('^END', '', trigger['statement'], flags=re.MULTILINE)
+                    trigger['statement'] = re.sub('`', '', trigger['statement'])
+
+                    self._triggers.append(trigger)
+
+        @property
+        def name(self):
+            return self._name
+
+        @property
+        def columns(self):
+            return self._columns
+
+        @property
+        def comment(self):
+            return self._comment
+
+        @property
+        def indexes(self):
+            return self._indexes
+
+        @property
+        def foreign_keys(self):
+            return self._foreign_keys
+
+        @property
+        def triggers(self):
+            return self._triggers
+
+        @property
+        def query_for(self):
+            return 'SELECT %(column_names)s FROM `%(table_name)s`' % {
+                'table_name': self.name,
+                'column_names': ', '. join(c['select'] for c in self.columns)}
+
+    def __init__(self, options):
+        self.db = DB(options)
+
+    @property
+    def tables(self):
+        return (self.Table(self, t[0]) for t in self.db.list_tables())
+
+    def read(self, table):
+        return self.db.query(table.query_for, large=True)
+
+    def close(self):
+        self.db.close()
---
+++ mysql2pgsql/lib/postgres_writer.py
@@ -0,0 +1,319 @@
+from __future__ import absolute_import
+
+import re
+from cStringIO import StringIO
+from datetime import date, datetime, timedelta
+
+from psycopg2.extensions import AsIs, Binary, QuotedString
+from pytz import timezone
+
+
+class PostgresWriter(object):
+    """Base class for :py:class:`mysql2pgsql.lib.postgres_file_writer.PostgresFileWriter`
+    and :py:class:`mysql2pgsql.lib.postgres_db_writer.PostgresDbWriter`.
+    """
+
+    def __init__(self, index_prefix, tz=False):
+        self.column_types = {}
+        self.index_prefix = index_prefix if index_prefix else ''
+        if tz:
+            self.tz = timezone('UTC')
+            self.tz_offset = '+00:00'
+        else:
+            self.tz = None
+            self.tz_offset = ''
+
+    def column_description(self, column):
+        return '"%s" %s' % (column['name'], self.column_type_info(column))
+
+    def column_type(self, column):
+        hash_key = hash(frozenset(column.items()))
+        self.column_types[hash_key] = self.column_type_info(column).split(" ")[0]
+        return self.column_types[hash_key]
+
+    def column_type_info(self, column):
+        """
+        """
+        null = "" if column['null'] else " NOT NULL"
+
+        def get_type(column):
+            """This in conjunction with :py:class:`mysql2pgsql.lib.mysql_reader.MysqlReader._convert_type`
+            determines the PostgreSQL data type. In my opinion this is way too fugly, will need
+            to refactor one day.
+            """
+            t = lambda v: not v == None
+            default = (' DEFAULT %s' % QuotedString(column['default']).getquoted()) if t(column['default']) else None
+
+            if column['type'] == 'char':
+                default = ('%s::char' % default) if t(default) else None
+                return default, 'character(%s)' % column['length']
+            elif column['type'] == 'varchar':
+                default = ('%s::character varying' % default) if t(default) else None
+                return default, 'character varying(%s)' % column['length']
+            elif column['type'] == 'integer':
+                default = (" DEFAULT %s" % (column['default'] if t(column['default']) else 'NULL')) if t(default) else None
+                return default, 'integer'
+            elif column['type'] == 'bigint':
+                default = (" DEFAULT %s" % (column['default'] if t(column['default']) else 'NULL')) if t(default) else None
+                return default, 'bigint'
+            elif column['type'] == 'tinyint':
+                default = (" DEFAULT %s" % (column['default'] if t(column['default']) else 'NULL')) if t(default) else None
+                return default, 'smallint'
+            elif column['type'] == 'boolean':
+                default = (" DEFAULT %s" % ('true' if int(column['default']) == 1 else 'false')) if t(default) else None
+                return default, 'boolean'
+            elif column['type'] == 'float':
+                default = (" DEFAULT %s" % (column['default'] if t(column['default']) else 'NULL')) if t(default) else None
+                return default, 'real'
+            elif column['type'] == 'float unsigned':
+                default = (" DEFAULT %s" % (column['default'] if t(column['default']) else 'NULL')) if t(default) else None
+                return default, 'real'
+            elif column['type'] in ('numeric', 'decimal'):
+                default = (" DEFAULT %s" % (column['default'] if t(column['default']) else 'NULL')) if t(default) else None
+                return default, 'numeric(%s, %s)' % (column['length'] or 20, column['decimals'] or 0)
+            elif column['type'] == 'double precision':
+                default = (" DEFAULT %s" % (column['default'] if t(column['default']) else 'NULL')) if t(default) else None
+                return default, 'double precision'
+            elif column['type'] == 'datetime' or column['type'].startswith('datetime('):
+                default = None
+                if self.tz:
+                    return default, 'timestamp with time zone'
+                else:
+                    return default, 'timestamp without time zone'
+            elif column['type'] == 'date':
+                default = None
+                return default, 'date'
+            elif column['type'] == 'timestamp':
+                if column['default'] == None:
+                    default = None
+                elif "current_timestamp()" in column['default']:
+                    default = ' DEFAULT CURRENT_TIMESTAMP'
+                elif "CURRENT_TIMESTAMP" in column['default']:
+                    default = ' DEFAULT CURRENT_TIMESTAMP'
+                elif "0000-00-00 00:00" in column['default']:
+                    if self.tz:
+                        default = " DEFAULT '1970-01-01T00:00:00.000000%s'" % self.tz_offset
+                    elif "0000-00-00 00:00:00" in column['default']:
+                        default = " DEFAULT '1970-01-01 00:00:00'"
+                    else:
+                        default = " DEFAULT '1970-01-01 00:00'"
+                if self.tz:
+                    return default, 'timestamp with time zone'
+                else:
+                    return default, 'timestamp without time zone'
+            elif column['type'] == 'time' or column['type'].startswith('time('):
+                default = " DEFAULT NOW()" if t(default) else None
+                if self.tz:
+                    return default, 'time with time zone'
+                else:
+                    return default, 'time without time zone'
+            elif column['type'] in ('blob', 'binary', 'longblob', 'mediumblob', 'tinyblob', 'varbinary'):
+                return default, 'bytea'
+            elif column['type'].startswith('binary(') or column['type'].startswith('varbinary('):
+                return default, 'bytea'
+            elif column['type'] in ('tinytext', 'mediumtext', 'longtext', 'text'):
+                return default, 'text'
+            elif column['type'].startswith('enum'):
+                default = (' %s::character varying' % default) if t(default) else None
+                enum = re.sub(r'^enum\(|\)$', '', column['type'])
+                # TODO: will work for "'.',',',''''" but will fail for "'.'',','.'"
+                max_enum_size = max([len(e.replace("''", "'")) for e in enum.split("','")])
+                return default, ' character varying(%s) check("%s" in (%s))' % (max_enum_size, column['name'], enum)
+            elif column['type'].startswith('bit('):
+                return ' DEFAULT %s' % column['default'].upper() if column['default'] else column['default'], 'varbit(%s)' % re.search(r'\((\d+)\)', column['type']).group(1)
+            elif column['type'].startswith('set('):
+                if default:
+                    default = ' DEFAULT ARRAY[%s]::text[]' % ','.join(QuotedString(
+                        v).getquoted() for v in re.search(r"'(.*)'", default).group(1).split(','))
+                return default, 'text[]'
+            else:
+                raise Exception('unknown %s' % column['type'])
+
+        default, column_type = get_type(column)
+
+        if column.get('auto_increment', None):
+            return '%s DEFAULT nextval(\'"%s_%s_seq"\'::regclass) NOT NULL' % (
+                   column_type, column['table_name'], column['name'])
+
+        return '%s%s%s' % (column_type, (default if not default == None else ''), null)
+
+    def table_comments(self, table):
+        comments = []
+        if table.comment:
+            comments.append('COMMENT ON TABLE %s is %s;' % (table.name, QuotedString(table.comment).getquoted()))
+        for column in table.columns:
+            if column['comment']:
+                comments.append('COMMENT ON COLUMN %s.%s is %s;' % (table.name, column['name'], QuotedString(column['comment']).getquoted()))
+        return comments
+
+    def process_row(self, table, row):
+        """Examines row data from MySQL and alters
+        the values when necessary to be compatible with
+        sending to PostgreSQL via the copy command
+        """
+        for index, column in enumerate(table.columns):
+            hash_key = hash(frozenset(column.items()))
+            column_type = self.column_types[hash_key] if hash_key in self.column_types else self.column_type(column)
+            if row[index] == None and ('timestamp' not in column_type or not column['default']):
+                row[index] = '\N'
+            elif row[index] == None and column['default']:
+                if self.tz:
+                    row[index] = '1970-01-01T00:00:00.000000' + self.tz_offset
+                else:
+                    row[index] = '1970-01-01 00:00:00'
+            elif 'bit' in column_type:
+                row[index] = bin(ord(row[index]))[2:]
+            elif isinstance(row[index], (str, unicode, basestring)):
+                if column_type == 'bytea':
+                    row[index] = Binary(row[index]).getquoted()[1:-8] if row[index] else row[index]
+                elif 'text[' in column_type:
+                    row[index] = '{%s}' % ','.join('"%s"' % v.replace('"', r'\"') for v in row[index].split(','))
+                else:
+                    row[index] = row[index].replace('\\', r'\\').replace('\n', r'\n').replace(
+                        '\t', r'\t').replace('\r', r'\r').replace('\0', '')
+            elif column_type == 'boolean':
+                # We got here because you used a tinyint(1), if you didn't want a bool, don't use that type
+                row[index] = 't' if row[index] not in (None, 0) else 'f' if row[index] == 0 else row[index]
+            elif isinstance(row[index], (date, datetime)):
+                if isinstance(row[index], datetime) and self.tz:
+                    try:
+                        if row[index].tzinfo:
+                            row[index] = row[index].astimezone(self.tz).isoformat()
+                        else:
+                            row[index] = datetime(*row[index].timetuple()[:6], tzinfo=self.tz).isoformat()
+                    except Exception as e:
+                        print e.message
+                else:
+                    row[index] = row[index].isoformat()
+            elif isinstance(row[index], timedelta):
+                row[index] = datetime.utcfromtimestamp(_get_total_seconds(row[index])).time().isoformat()
+            else:
+                row[index] = AsIs(row[index]).getquoted()
+
+    def table_attributes(self, table):
+        primary_keys = []
+        serial_key = None
+        maxval = None
+        columns = StringIO()
+
+        for column in table.columns:
+            if column['auto_increment']:
+                serial_key = column['name']
+                maxval = 1 if column['maxval'] < 1 else column['maxval'] + 1
+            if column['primary_key']:
+                primary_keys.append(column['name'])
+            columns.write('  %s,\n' % self.column_description(column))
+        return primary_keys, serial_key, maxval, columns.getvalue()[:-2]
+
+    def truncate(self, table):
+        serial_key = None
+        maxval = None
+
+        for column in table.columns:
+            if column['auto_increment']:
+                serial_key = column['name']
+                maxval = 1 if column['maxval'] < 1 else column['maxval'] + 1
+
+        truncate_sql = 'TRUNCATE "%s" CASCADE;' % table.name
+        serial_key_sql = None
+
+        if serial_key:
+            serial_key_sql = "SELECT pg_catalog.setval(pg_get_serial_sequence(%(table_name)s, %(serial_key)s), %(maxval)s, true);" % {
+                'table_name': QuotedString('"%s"' % table.name).getquoted(),
+                'serial_key': QuotedString(serial_key).getquoted(),
+                'maxval': maxval}
+
+        return (truncate_sql, serial_key_sql)
+
+    def write_table(self, table):
+        primary_keys, serial_key, maxval, columns = self.table_attributes(table)
+        serial_key_sql = []
+        table_sql = []
+        if serial_key:
+            serial_key_seq = '%s_%s_seq' % (table.name, serial_key)
+            serial_key_sql.append('DROP SEQUENCE IF EXISTS "%s" CASCADE;' % serial_key_seq)
+            serial_key_sql.append("""CREATE SEQUENCE "%s" INCREMENT BY 1
+                                  NO MAXVALUE NO MINVALUE CACHE 1;""" % serial_key_seq)
+            serial_key_sql.append('SELECT pg_catalog.setval(\'"%s"\', %s, true);' % (serial_key_seq, maxval))
+
+        table_sql.append('DROP TABLE IF EXISTS "%s" CASCADE;' % table.name)
+        table_sql.append('CREATE TABLE "%s" (\n%s\n)\nWITHOUT OIDS;' % (table.name.encode('utf8'), columns))
+        table_sql.extend(self.table_comments(table))
+        return (table_sql, serial_key_sql)
+
+    def write_indexes(self, table):
+        index_sql = []
+        primary_index = [idx for idx in table.indexes if idx.get('primary', None)]
+        index_prefix = self.index_prefix
+        if primary_index:
+            index_sql.append('ALTER TABLE "%(table_name)s" ADD CONSTRAINT "%(index_name)s_pkey" PRIMARY KEY(%(column_names)s);' % {
+                'table_name': table.name,
+                'index_name': '%s%s_%s' % (index_prefix, table.name,
+                                           '_'.join(primary_index[0]['columns'])),
+                'column_names': ', '.join('"%s"' % col for col in primary_index[0]['columns']),
+            })
+        for index in table.indexes:
+            if 'primary' in index:
+                continue
+            unique = 'UNIQUE ' if index.get('unique', None) else ''
+            index_name = '%s%s_%s' % (index_prefix, table.name, '_'.join(index['columns']))
+            index_sql.append('DROP INDEX IF EXISTS "%s" CASCADE;' % index_name)
+            index_sql.append('CREATE %(unique)sINDEX "%(index_name)s" ON "%(table_name)s" (%(column_names)s);' % {
+                'unique': unique,
+                'index_name': index_name,
+                'table_name': table.name,
+                'column_names': ', '.join('"%s"' % col for col in index['columns']),
+            })
+
+        return index_sql
+
+    def write_constraints(self, table):
+        constraint_sql = []
+        for key in table.foreign_keys:
+            constraint_sql.append("""ALTER TABLE "%(table_name)s" ADD FOREIGN KEY ("%(column_name)s")
+            REFERENCES "%(ref_table_name)s"(%(ref_column_name)s);""" % {
+                'table_name': table.name,
+                'column_name': key['column'],
+                'ref_table_name': key['ref_table'],
+                'ref_column_name': key['ref_column']})
+        return constraint_sql
+
+    def write_triggers(self, table):
+        trigger_sql = []
+        for key in table.triggers:
+            trigger_sql.append("""CREATE OR REPLACE FUNCTION %(fn_trigger_name)s RETURNS TRIGGER AS $%(trigger_name)s$
+            BEGIN
+                %(trigger_statement)s
+            RETURN NULL;
+            END;
+            $%(trigger_name)s$ LANGUAGE plpgsql;""" % {
+                'table_name': table.name,
+                'trigger_time': key['timing'],
+                'trigger_event': key['event'],
+                'trigger_name': key['name'],
+                'fn_trigger_name': 'fn_' + key['name'] + '()',
+                'trigger_statement': key['statement']})
+
+            trigger_sql.append("""CREATE TRIGGER %(trigger_name)s %(trigger_time)s %(trigger_event)s ON %(table_name)s
+            FOR EACH ROW
+            EXECUTE PROCEDURE fn_%(trigger_name)s();""" % {
+                'table_name': table.name,
+                'trigger_time': key['timing'],
+                'trigger_event': key['event'],
+                'trigger_name': key['name']})
+
+        return trigger_sql
+
+    def close(self):
+        raise NotImplementedError
+
+    def write_contents(self, table, reader):
+        raise NotImplementedError
+
+# Original fix for Py2.6: https://github.com/mozilla/mozdownload/issues/73
+def _get_total_seconds(dt):
+    # Keep backward compatibility with Python 2.6 which doesn't have this method
+    if hasattr(datetime, 'total_seconds'):
+        return dt.total_seconds()
+    else:
+        return (dt.microseconds + (dt.seconds + dt.days * 24 * 3600) * 10**6) / 10**6
